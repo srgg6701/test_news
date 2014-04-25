604b541f30a185d7c7fa7ef84eb05dbd2dc9849f
srgg6701 <srgg67@gmail.com>
2014-04-25 18:54:00 +0400

    Рефакторинг, добавление статического списка городов.

--- views/default.php
+++ views/default.php
@@ -1,11 +1,11 @@
 <section class="clearfix">
-    <div class="floatLeft">
+    <div class="floatLeft padding10 marginRight10 bg-lightskyblue">
         <h3>Чтение новостей</h3>
         <select>
             <option>-Выберите регион-</option>
         </select>
     </div>
-    <div class="floatLeft">
+    <div class="floatLeft padding10">
         <h3>Администрирование новостей</h3>
         <a href="/add">Добавить новость</a>
     </div>
